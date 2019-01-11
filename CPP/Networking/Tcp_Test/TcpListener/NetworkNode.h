2b5817f497000b69c43f6d473957c632ef0f4e78
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-01-10 22:06:26 -0500

    Finished constructing listener class

--- CPP/Networking/Tcp_Test/TcpListener/NetworkNode.h
+++ CPP/Networking/Tcp_Test/TcpListener/NetworkNode.h
@@ -10,12 +10,17 @@
 #include <WS2tcpip.h>
 #include <string>
 
+#ifndef NETWORKNODE_H
+#define NETWORKNODE_H
+
 // struct containing network information
 struct NetworkNode {
-	SOCKET socket;									// socket object
-	sockaddr_in hint;								// connection information
-	std::map<std::string, std::string> attributes;	// Ma with node attributes (customly defined)
+	SOCKET								socket;		// socket object
+	sockaddr_in							hint;		// connection information
+	std::map<std::string, std::string>	attributes;	// Ma with node attributes (customly defined)
 
 	char* getName();								// retrieves connection name of node
 	int getPort();									// retrieves port the node is listening on
 };
+
+#endif // !NETWORKNODE_H
