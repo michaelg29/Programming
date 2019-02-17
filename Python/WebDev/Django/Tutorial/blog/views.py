c8c3530536f3de4cc01d414367aced0b51e91719
michaelg29 <michaelgriecocoding@gmail.com>
2019-02-17 17:58:54 -0500

    Added conten

--- Python/WebDev/Django/Tutorial/blog/views.py
+++ Python/WebDev/Django/Tutorial/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+
+def login(request):
+    return HttpResponse('<h1>Login</h1>')
