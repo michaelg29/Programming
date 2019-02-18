afad51d8acfbae366569f885de6b0f613fff6976
Michael Grieco <modularglitch@gmail.com>
2019-02-17 22:09:45 -0500

    Created new project

--- Python/WebDev/Django/MundusInvicte/MundusInvicte/wsgi.py
+++ Python/WebDev/Django/MundusInvicte/MundusInvicte/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MDT project.
+WSGI config for MundusInvicte project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MDT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MundusInvicte.settings')
 
 application = get_wsgi_application()
