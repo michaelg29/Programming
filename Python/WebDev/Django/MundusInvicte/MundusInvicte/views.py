ce9bf097cd6a263865571ef464709faa4d01b935
Michael Grieco <modularglitch@gmail.com>
2019-02-18 18:43:49 -0500

    created users portion of app

--- Python/WebDev/Django/MundusInvicte/MundusInvicte/views.py
+++ Python/WebDev/Django/MundusInvicte/MundusInvicte/views.py
@@ -3,9 +3,7 @@ from django.shortcuts import render
 from . import context_processors
 
 def home(request):
-    context_processors.editContext('loggedIn', True)
     return render(request, 'MundusInvicte/home.html')
 
 def about(request):
-    context_processors.editContext('loggedIn', False)
     return render(request, 'MundusInvicte/about.html')
