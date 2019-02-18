c9bea4a1f2397940fbbb7b8edf77d38a4aca0809
Michael Grieco <modularglitch@gmail.com>
2019-02-17 23:43:16 -0500

    Added content

---
+++ Python/WebDev/Django/MundusInvicte/MDT/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MdtConfig(AppConfig):
+    name = 'MDT'
--- Python/WebDev/Django/MundusInvicte/MundusInvicte/settings.py
+++ Python/WebDev/Django/MundusInvicte/MundusInvicte/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MDT.apps.MdtConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
