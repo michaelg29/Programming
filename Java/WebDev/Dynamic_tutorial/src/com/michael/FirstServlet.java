2660a212b435344d173e2042f987bd9d718b5cf5
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-02-19 10:13:33 -0500

    Created dynamic web project

---
+++ Java/WebDev/Dynamic_tutorial/src/com/michael/FirstServlet.java
@@ -0,0 +1,51 @@
+package com.michael;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String HTML_START = "<html><body>";
+	public static final String HTML_END = "</body></html";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher view = request.getRequestDispatcher("html/input.html");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name"),
+				address = request.getParameter("address"),
+				phone = request.getParameter("phone");
+		
+		request.getSession().setAttribute("name", name);
+		request.getSession().setAttribute("address", address);
+		request.getSession().setAttribute("phone", phone);
+		
+		response.sendRedirect("jsp/output.jsp");
+	}
+}
