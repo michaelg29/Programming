da5edf6f52666a9bb87fc375fd32a8514691249b
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-02-18 21:27:05 -0500

    Added content

--- Python/WebDev/Django/Tutorial/users/views.py
+++ Python/WebDev/Django/Tutorial/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
@@ -9,11 +9,10 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
-        
     return render(request, 'users/register.html', {'form': form})
 
 @login_required
