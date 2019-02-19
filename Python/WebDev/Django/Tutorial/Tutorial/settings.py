769178397509895ce2afee19e76053225806c1ed
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-02-18 19:11:21 -0500

    Added content

--- Python/WebDev/Django/Tutorial/Tutorial/settings.py
+++ Python/WebDev/Django/Tutorial/Tutorial/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+# admin - michaelgrieco - admin123
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -33,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # ========================== add new apps here
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
