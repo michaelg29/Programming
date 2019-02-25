812286d49d380cf2d6be8a63c68f1d922fcbfdf5
Michael Grieco <modularglitch@gmail.com>
2019-02-24 23:10:31 -0500

    Added basic window functionality

--- CPP/GameDev/First3D/First3D/libs.h
+++ CPP/GameDev/First3D/First3D/libs.h
@@ -9,3 +9,5 @@
 #include <mat4x4.hpp>
 #include <gtc\matrix_transform.hpp>
 #include <gtc\type_ptr.hpp>
+
+#include <SOIL2.h>
--- CPP/GameDev/First3D/First3D/main.cpp
+++ CPP/GameDev/First3D/First3D/main.cpp
@@ -1,17 +1,72 @@
 #include "libs.h"
 
+// https://www.youtube.com/playlist?list=PL6xSOsbVA1eYSZTKBxnoXYboy7wc4yg-Z part 9
+
+void framebuffer_resize_callback(GLFWwindow* widnow, int fbW, int fbH) {
+	glViewport(0, 0, fbW, fbH);
+}
+
 int main() {
+	// initialize GLFW
 	glfwInit();
 
-	glm::vec3(0.f);
+	// create window
+	const int WINDOW_WIDTH = 640;
+	const int WINDOW_HEIGHT = 480;
+	int framebufferWidth = 0;
+	int framebufferHeight = 0;
+	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
-	// GLEW
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); Mac os
+	
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tutorial", NULL, NULL);
+
+	// For static window size: glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+	//glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+	// For dynamic window size:
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+
+	glfwMakeContextCurrent(window); // Important
+
+	// init glew (Needs window and OPENGL context)
 	glewExperimental = GL_TRUE;
 
 	if (glewInit() != GLEW_OK) {
+		std::cout << "ERROR::MAIN.CPP::GLEW_INIT_FAILED" << std::endl;
 		glfwTerminate();
-		throw "Error in GLEW Init";
 	}
+
+	// main loop
+	bool gameRunning = true;
+
+	while (!glfwWindowShouldClose(window)) {
+		// update input
+		glfwPollEvents(); // takes all events in queue
+
+		// glfwSetWindowShouldClose(window, true);
+
+		// UPDATE ---
+
+		// DRAW ---
+		// clear
+		glClearColor(0.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		// draw
+
+		// end draw
+		glfwSwapBuffers(window);
+		glFlush();
+	}
+
+	// end of program
+	glfwTerminate();
 
 	system("pause");
 	return 0;
