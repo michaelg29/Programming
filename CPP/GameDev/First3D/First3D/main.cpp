1559fed458b6033dd669d8d7ecdb58dd592f0940
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-03-01 14:26:59 -0500

    Added ability to draw other polygons

--- CPP/GameDev/First3D/First3D/main.cpp
+++ CPP/GameDev/First3D/First3D/main.cpp
@@ -4,14 +4,16 @@
 
 Vertex vertices[] = {
 	// Position							// color						// texcoords
-	glm::vec3(0.0f, 0.5f, 0.f),			glm::vec3(1.f, 0.f, 0.f),		glm::vec2(0.f, 1.f),
+	glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),		glm::vec2(0.f, 1.f),
 	glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),		glm::vec2(0.f, 0.f),
-	glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),		glm::vec2(1.f, 0.f)
+	glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),		glm::vec2(1.f, 0.f),
+	glm::vec3(0.5f, 0.5f, 0.f),			glm::vec3(1.f, 1.f, 0.f),		glm::vec2(0.f, 0.f),
 };
 unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
 GLuint indices[] = {
-	0, 1, 2
+	0, 1, 2,	// triangle 1
+	2, 3, 0		// triangle 2
 };
 unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
 
@@ -127,6 +129,8 @@ int main() {
 	// initialize GLFW
 	glfwInit();
 
+	glm::vec4 v(1, 1, 1, 1);
+
 	// create window
 	const int WINDOW_WIDTH = 640;
 	const int WINDOW_HEIGHT = 480;
@@ -240,9 +244,8 @@ int main() {
 		glBindVertexArray(VAO);
 
 		// draw
-		//glDrawElements(GL_TRIANGLES, nrOfIndices, GL_UNSIGNED_INT, 0);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glRectf(0.5f, 0.5f, -0.5f, -0.5f);
+		//glDrawArrays(GL_TRIANGLES, 0, nrOfVertices);
+		glDrawElements(GL_TRIANGLES, nrOfIndices, GL_UNSIGNED_INT, 0);
 
 		// end draw
 		glfwSwapBuffers(window);
