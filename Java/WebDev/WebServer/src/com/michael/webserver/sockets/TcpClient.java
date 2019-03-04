d49a82555c46b4a0c61470e364aba5d4b183bb5b
Michael Grieco <modularglitch@gmail.com>
2019-03-04 00:53:44 -0500

    Redid and fixed Tcp server and client classes

---
+++ Java/WebDev/WebServer/src/com/michael/webserver/sockets/TcpClient.java
@@ -0,0 +1,24 @@
+package com.michael.webserver.sockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TcpClient {
+	protected Socket socket;
+	protected PrintWriter writer;
+	protected BufferedReader reader;
+	
+	public TcpClient(Socket socket) throws IOException {
+		this.socket = socket;
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintWriter(socket.getOutputStream(), true);
+        this.writer.println("Welcome to the server");
+	}
+	
+	public void send(String msg) {
+		this.writer.println(msg);
+	}
+}
--- Java/WebDev/WebServer/src/com/test/webserver/Main.java
+++ Java/WebDev/WebServer/src/com/test/webserver/Main.java
@@ -1,11 +1,14 @@
 package com.test.webserver;
 
+import java.io.IOException;
+
 import com.michael.webserver.sockets.TcpListener;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		WebServerLogger logger = new WebServerLogger();
-		TcpListener tcp = new TcpListener("localhost", 9000, logger);
+		TcpListener tcp = new TcpListener(9000, "localhost", logger);
+		tcp.bind();
 		tcp.run();
 	}
 }
