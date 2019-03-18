7affb52d9255a7d6e58d43e609b6441b4d92bdad
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-03-18 11:31:03 -0400

    Updated link

--- Python/WebDev/Flask/MundusInvicte/user.py
+++ Python/WebDev/Flask/MundusInvicte/user.py
@@ -1,5 +1,5 @@
 from flask import Flask, session, redirect, url_for, request, render_template, Blueprint
-from util import setMessage, getURL
+from .util import setMessage, getURL
 import json
 
 user_pages = Blueprint('user_pages', __name__, template_folder='templates')
