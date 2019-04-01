2e03e7d7868ee9014105658e4cceddbd02d1683b
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-04-01 14:21:07 -0400

    Added context methods

--- Python/WebDev/WebServer/start.py
+++ Python/WebDev/WebServer/start.py
@@ -6,12 +6,20 @@ def home(request):
         request.client.context["name"] = request.params["name"]
     request.render_template("index.html")
 
+def dosomething():
+    print("Context method")
+
 if __name__ == "__main__":
     ws = WebServer("127.0.0.1", 8080)
 
     ws.routes = { 
         "" : home,
     }
+
+    context_methods = {
+        "print" : dosomething
+    }
+    ws.registerContextMethods(context_methods)
 
     ws.routes.update(HomeApp().getRoutes())
 
