7277795755af142d34aacf6b1e4ced655ca75cd9
modularglitch <modularglitch@gmail.com>
2019-04-20 16:29:00 -0400

    Added content

---
+++ CPP/GameDev/First3D/YT_Tutorial/fragment_core.glsl
@@ -0,0 +1,70 @@
+#version 440
+
+struct Material
+{
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+	sampler2D diffuseTex;
+	sampler2D specularTex;
+};
+
+in vec3 vs_position;
+in vec3 vs_color;
+in vec2 vs_texcoord;
+in vec3 vs_normal;
+
+out vec4 fs_color;
+
+//Uniforms
+uniform Material material;
+uniform vec3 lightPos0;
+uniform vec3 cameraPos;
+
+//Functions
+vec3 calculateAmbient(Material material)
+{
+	return material.ambient;
+}
+
+vec3 calculateDiffuse(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0)
+{
+	vec3 posToLightDirVec = normalize(lightPos0 - vs_position);
+	float diffuse = clamp(dot(posToLightDirVec, vs_normal), 0, 1);
+	vec3 diffuseFinal = material.diffuse * diffuse;
+
+	return diffuseFinal;
+}
+
+vec3 calculateSpecular(Material material, vec3 vs_position, vec3 vs_normal, vec3 lightPos0, vec3 cameraPos)
+{
+	vec3 lightToPosDirVec = normalize(vs_position - lightPos0);
+	vec3 reflectDirVec = normalize(reflect(lightToPosDirVec, normalize(vs_normal)));
+	vec3 posToViewDirVec = normalize(cameraPos - vs_position);
+	float specularConstant = pow(max(dot(posToViewDirVec, reflectDirVec), 0), 35);
+	vec3 specularFinal = material.specular * specularConstant * texture(material.specularTex, vs_texcoord).rgb;
+
+	return specularFinal;
+}
+
+void main()
+{
+	//fs_color = vec4(vs_color, 1.f);
+	//fs_color = texture(texture0, vs_texcoord) * texture(texture1, vs_texcoord) * vec4(vs_color, 1.f);
+	
+	//Ambient light
+	vec3 ambientFinal = calculateAmbient(material);
+
+	//Diffuse light
+	vec3 diffuseFinal = calculateDiffuse(material, vs_position, vs_normal, lightPos0);
+
+	//Specular light
+	vec3 specularFinal = calculateSpecular(material, vs_position, vs_normal, lightPos0, cameraPos);
+
+	//Attenuation
+
+	//Final light
+	fs_color = 
+	texture(material.diffuseTex, vs_texcoord)
+	* (vec4(ambientFinal, 1.f) + vec4(diffuseFinal, 1.f) + vec4(specularFinal, 1.f));
+}
---
+++ CPP/GameDev/First3D/YT_Tutorial/libs.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+#include <GL/glew.h>
+#include<glfw3.h>
+
+#include<glm.hpp>
+#include<vec2.hpp>
+#include<vec3.hpp>
+#include<vec4.hpp>
+#include<mat4x4.hpp>
+#include<gtc\matrix_transform.hpp>
+#include<gtc\type_ptr.hpp>
+
+#include<SOIL2.h>
+
+#include"Vertex.h"
+#include"Primitives.h"
+#include"Shader.h"
+#include"Texture.h"
+#include"Material.h"
+#include"Mesh.h"
+#include"Model.h"
