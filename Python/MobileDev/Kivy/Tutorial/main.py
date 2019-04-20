ce731b63583eca3fdcad62f8afdb198bf9d79aca
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-04-20 16:37:40 -0400

    Reformatted code

--- Python/MobileDev/Kivy/Tutorial/main.py
+++ Python/MobileDev/Kivy/Tutorial/main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.uix.label import Label
 
-class FirstKivy(App):
+class MyApp(App):
     def build(self):
         return Label(text="Hello Kivy!")
 
-FirstKivy().run()
+if __name__ == "__main__":
+    MyApp().run()
