cd75bba66aa41b7089401e983df98e52effbece9
Michael Grieco <michaelgriecocoding@gmail.com>
2019-04-22 21:57:55 -0400

    define element array for indices

--- Web/WebGL/Tutorial/main.js
+++ Web/WebGL/Tutorial/main.js
@@ -199,9 +199,27 @@ function initBuffers(gl) {
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    // array defines face as two triangles
+    const indices = [
+        0,  1,  2,      0,  2,  3,    // front
+        4,  5,  6,      4,  6,  7,    // back
+        8,  9,  10,     8,  10, 11,   // top
+        12, 13, 14,     12, 14, 15,   // bottom
+        16, 17, 18,     16, 18, 19,   // right
+        20, 21, 22,     20, 22, 23,   // left
+    ];
+
+    // send array to gl
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(indices), gl.STATIC_DRAW);
+
     return {
         position: positionBuffer,
         color: colorBuffer,
+        indices: indexBuffer,
     }
 }
 
