8976f59707b32552e1f269d3727ae5fc5f632e75
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2019-07-02 10:10:58 -0400

    Updated app name

--- Mobile/Flutter/quiz/lib/main.dart
+++ Mobile/Flutter/quiz/lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
 import './pages/landing_page.dart';
-import './pages/quiz_page.dart';
-import './pages/score_page.dart';
 
 void main() {
   runApp(new MaterialApp(
+    title: 'Quiz App',
     home: new LandingPage(),
   ));
 }
