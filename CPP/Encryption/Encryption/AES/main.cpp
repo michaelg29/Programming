c73678613c7b41f6c55423f5aa423deb7cd94552
michaelg29 <michaelgriecocoding@gmail.com>
2019-07-09 00:43:45 -0400

    Created skeleton for algorithm

--- CPP/Encryption/Encryption/AES/main.cpp
+++ CPP/Encryption/Encryption/AES/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+void keyExpansion() {}
+void initialRound() {}
+
+void subBytes() {}
+void shiftRows() {}
+void mixColumns() {}
+void addRoundKey() {}
+
+void encrypt() {
+	int numberOfRounds = 1;
+
+	keyExpansion();
+	initialRound(); // whitening/add round key
+
+	for (int i = 0; i < numberOfRounds; i++) {
+		subBytes();
+		shiftRows();
+		mixColumns();
+		addRoundKey();
+	}
+
+	// final round
+	subBytes();
+	shiftRows();
+	addRoundKey();
+}
+
 int main() {
 	std::cout << "AES Encryption" << std::endl;
+
+	char message[] = "This is a message we will encrypt";
+	char key[16] = {
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+	};
+
+	encrypt();
 }
