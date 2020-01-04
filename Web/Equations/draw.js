1db16b9e351bb89e289cfe08cc3e46e828d05fd3
michaelg29 <michaelgriecocoding@gmail.com>
2020-01-03 19:08:47 -0500

    Fixed motion control

--- Web/Equations/draw.js
+++ Web/Equations/draw.js
@@ -5,30 +5,21 @@ btn = document.querySelector('#btn');
 var canvas = document.querySelector("#canvas");
 var ctx = canvas.getContext("2d");
 
+var rect = canvas.getBoundingClientRect();
+
 //ctx.lineWidth = 1;
 ctx.translate(0, canvas.height);
 ctx.rotate(Math.PI);
 ctx.scale(-1, 1);
 
 let equations = [];
-let global_scale = 1;
+let global_scale = 100; // pixels per unit on axis
 
-function draw(tree) {
-    variables.X = 0;
-    let previousY = eval(tree);
+var minX = -1;
+var minY = -2;
+var dx;
 
-    for (var i = 0, width = canvas.width; i < width; i++) {
-        variables.X = minX + i * global_scale;
-        let y = eval(tree);
-
-        ctx.beginPath();
-        ctx.moveTo(variables.X - 1, previousY);
-        ctx.lineTo(variables.X, y);
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        previousY = y;
-    }
-}
+var mouse_down;
 
 btn.addEventListener('click', (e) => {
     let eqn = inelement.value;
@@ -51,37 +42,104 @@ btn.addEventListener('click', (e) => {
     outelement.innerHTML = out;
 });
 
-let mousedown = false;
+canvas.addEventListener("mousedown", (e) => {
+    mouse_down = true;
+    lastX = e.clientX - rect.left;
+    lastY = rect.bottom - e.clientY;
+});
+canvas.addEventListener("mouseup", () => mouse_down = false);
+
 var lastX;
 var lastY;
-var minX = 0;
-var minY = 0;
 
-document.addEventListener("mousedown", (e) => mousedown = true);
-document.addEventListener("mouseup", (e) => mousedown = false);
-drawAxes();
+canvas.addEventListener("mousemove", (e) => {
+    var cursor_x = e.clientX - rect.left;
+    var cursor_y = rect.bottom - e.clientY;
 
-function clearCanvas() {
-    ctx.clearRect(minX, minY, canvas.width, canvas.height);
-    outelement.innerHTML = `${minX} ${minY}`;
+    outelement.innerHTML = `${minX + cursor_x / global_scale} ${minY + cursor_y / global_scale}`;
+
+    if (!mouse_down) {
+        return;
+    }
+
+    // translate graph
+    var _dx = cursor_x - lastX;
+    var _dy = cursor_y - lastY;
+
+    minX -= _dx / global_scale;
+    minY -= _dy / global_scale;
+
+    lastX = cursor_x;
+    lastY = cursor_y;
+
+    redraw();
+});
+
+canvas.addEventListener("wheel", (e) => {
+    var dy = -e.deltaY;
+
+    var cursor_x = e.clientX - rect.left;
+    var cursor_y = rect.bottom - e.clientY;
+    var x = minX + cursor_x / global_scale;
+    var y = minY + cursor_y / global_scale;
+
+    outelement.innerHTML = `${minX + cursor_x / global_scale} ${minY + cursor_y / global_scale}`;
+
+    if (dy > 0) {
+        // scale up
+        global_scale *= 1.05;
+    } else if (dy < 0) {
+        // scale down
+        global_scale *= .95;
+    }
+
+    minX = x - cursor_x / global_scale;
+    minY = y - cursor_y / global_scale;
+
+    redraw();
+});
+
+function draw(tree) {
+    variables.X = minX;
+    let previousY = eval(tree);
+
+    dx = 1 / global_scale;
+
+    for (var i = 1, width = canvas.width; i < width; i++) {
+        variables.X = minX + i * dx;
+        let y = (eval(tree) - minY) * global_scale;
+
+        ctx.beginPath();
+        ctx.moveTo(i - 1, previousY);
+        ctx.lineTo(i, y);
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        previousY = y;
+    }
 }
 
 function drawAxes() {
-    if (minX < canvas.width && minX > -canvas.width) {
+    if (minX >= -canvas.width / global_scale && minX <= canvas.width / global_scale) {
+        // draw y-axis
         ctx.beginPath();
-        ctx.moveTo(0, minY);
-        ctx.lineTo(0, minY + canvas.height);
+        ctx.moveTo(-minX * global_scale, 0);
+        ctx.lineTo(-minX * global_scale, canvas.height);
         ctx.lineWidth = 5;
         ctx.stroke();
     }
 
-    if (minY < canvas.height && minY > -canvas.height) {
+    if (minY >= -canvas.height / global_scale && minY <= canvas.height / global_scale) {
+        // draw x-axis
         ctx.beginPath();
-        ctx.moveTo(minX, 0);
-        ctx.lineTo(minX + canvas.width, 0);
+        ctx.moveTo(0, -minY * global_scale);
+        ctx.lineTo(canvas.width, -minY * global_scale);
         ctx.lineWidth = 5;
         ctx.stroke();
     }
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function redraw() {
@@ -92,45 +150,10 @@ function redraw() {
     Array.from(equations).forEach((eqn) => draw(eqn.tree));
 }
 
-canvas.addEventListener("mousemove", (e) => {
-    if (mousedown) {
-        var dx = lastX - e.clientX;
-        var dy = lastY - e.clientY;
+function reset() {
+    equations = [];
 
-        ctx.translate(-dx, dy);
-        lastX = e.clientX;
-        lastY = e.clientY;
+    redraw();
+}
 
-        minX += dx;
-        minY -= dy;
-
-        // clear canvas
-        redraw();
-    }
-});
-
-canvas.addEventListener("mousedown", (e) => {
-    lastX = e.clientX;
-    lastY = e.clientY;
-});
-
-// canvas.addEventListener("wheel", (e) => {
-//     var dy = e.deltaY;
-//     var scale = Math.abs(dy) / 50;
-//     let x = e.clientX;
-//     let y = e.clientY;
-
-//     if (dy > 0) {
-//         // scale up
-//         minX += x * scale;
-//         minY += y * scale;
-//         global_scale -= scale;
-//     } else if (dy < 0) {
-//         // scale down
-//         minX -= x * scale;
-//         minY -= y * scale;
-//         global_scale += scale;
-//     }
-
-//     clearCanvas();
-// });
+drawAxes();
