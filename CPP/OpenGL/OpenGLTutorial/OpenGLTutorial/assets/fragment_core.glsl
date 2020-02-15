1b3b2c79124877f124d48309567ed80877727689
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-02-15 11:30:15 -0500

    Added textures and modified shader program

---
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+out vec4 FragColor;
+
+uniform vec4 ourColor; // set in code
+in vec2 TexCoord;
+
+uniform float mixValue; // set in code
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	FragColor = ourColor;
+	//FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), mixValue); // 20% of first, 80% of second
+}
