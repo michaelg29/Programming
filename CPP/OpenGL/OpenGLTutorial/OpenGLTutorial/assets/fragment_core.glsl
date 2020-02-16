95886165dbbda2ccd7485b045e7140e6d4f3200e
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-02-16 17:40:56 -0500

    Added transformations

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/fragment_core.glsl
@@ -1,8 +1,8 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec4 ourColor; // set in code
-//in vec3 ourColor;
+//uniform vec4 ourColor; // set in code
+in vec3 ourColor;
 in vec2 TexCoord;
 
 uniform float mixValue; // set in code
@@ -12,6 +12,6 @@ uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = ourColor;
-	//FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), mixValue); // 20% of first, 80% of second
+	//FragColor = ourColor;
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), mixValue); // 20% of first, 80% of second
 }
--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
@@ -3,14 +3,17 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
 
-uniform float offset;
+//uniform float offset;
 
 out vec3 ourColor;
 out vec2 TexCoord;
 
+uniform mat4 transform;
+
 void main()
 {
-	gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
+	//gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
+	gl_Position = transform * vec4(aPos, 1.0f);
 	ourColor = aColor;
 	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
