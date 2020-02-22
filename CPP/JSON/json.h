99c09288b19ecd548dcde2caaf9f22119d56b7e6
michaelg29 <michaelgriecocoding@gmail.com>
2020-02-22 11:22:54 -0500

    Fixed writing method and added modifiable values

--- CPP/JSON/json.h
+++ CPP/JSON/json.h
@@ -36,6 +36,15 @@ namespace json {
 		json_type type;
 		std::string getType();
 
+		void setVal(std::string val) { s_val = val; type = json_type::json_string; }
+		void setVal(const char* val) { s_val = val; type = json_type::json_string; }
+		void setVal(double val) { n_val = val; type = json_type::json_num; }
+		void setVal(int val) { n_val = val; type = json_type::json_num; }
+		void setVal(float val) { n_val = val; type = json_type::json_num; }
+		void setVal(bool val) { b_val = val; type = json_type::json_bool; }
+		void setVal(std::vector<json_data> val) { l_val = val; type = json_type::json_list; }
+		void setVal(std::map<std::string, json_data> val) { o_val = val; type = json_type::json_object; }
+
 	protected:
 		std::string s_val;
 		double n_val;
@@ -49,6 +58,9 @@ namespace json {
 		json_string(std::string val)
 			: json_data(val) {}
 
+		json_string(json_data d)
+			: json_data(d) {}
+
 		std::string stringify();
 		std::string val() { return s_val; }
 	};
@@ -57,6 +69,9 @@ namespace json {
 	public:
 		json_num(double val)
 			: json_data(val) {}
+
+		json_num(json_data d)
+			: json_data(d) {}
 
 		std::string stringify();
 		double val() { return n_val; }
@@ -70,6 +85,9 @@ namespace json {
 		json_bool(bool val)
 			: json_data(val) {}
 
+		json_bool(json_data d)
+			: json_data(d) {}
+
 		std::string stringify();
 		bool val() { return b_val; }
 	};
@@ -79,8 +97,17 @@ namespace json {
 		json_list(std::vector<json_data> val)
 			: json_data(val) {}
 
+		json_list(json_data d)
+			: json_data(d) {}
+
 		std::string stringify();
 		std::vector<json_data> val() { return l_val; }
+
+		void push(json_data val) { l_val.push_back(val); }
+
+		json_data* operator[](int idx) {
+			return &(l_val[idx]);
+		}
 	};
 
 	class json_object : public json_data {
@@ -88,18 +115,21 @@ namespace json {
 		json_object(std::map<std::string, json_data> val)
 			: json_data(val) {}
 
+		json_object(json_data d)
+			: json_data(d) {}
+
 		std::string stringify();
 		std::map<std::string, json_data> val() { return o_val; }
+
+		json_data* operator[](std::string key) {
+			return &(o_val[key]);
+		}
 	};
 
 	class json_util {
 	public:
-		static json_data parse(std::string json_str);
-
 		static json_data read(const char* filename);
 		static std::string readFile(const char* filename);
-
-		static std::string stringify(json_data data);
 
 		static bool write(const char* filename, json_data val);
 		static bool writeFile(const char* filename, std::string content);
--- CPP/JSON/main.cpp
+++ CPP/JSON/main.cpp
@@ -8,8 +8,14 @@ using namespace json;
 int main() {
 	std::cout << "Hello, JSON!" << std::endl;
 
-	json_data d = json_util::read("data.json");
-	std::cout << d.getType() << ' ' << d.stringify() << std::endl;
+	json_object d = json_util::read("data.json");
+	//std::cout << d.getType() << ' ' << d.stringify() << std::endl;
+
+	//json_util::write("data2.json", d);
+
+	d["json"]->setVal(false);
+
+	std::cout << d.stringify() << std::endl;
 
 	//std::string data = "{\"hello\": 3, \"name\": true}";
 	//json_data jdata = json_data::parse(data);
