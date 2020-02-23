688211aea66751c35aca82dbda6cbf05c16d3e8a
michaelg29 <michaelgriecocoding@gmail.com>
2020-02-22 22:44:05 -0500

    Added existing content

--- Mobile/react_native/RnTodo/App.js
+++ Mobile/react_native/RnTodo/App.js
@@ -1,49 +1,22 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import { FileSystem } from 'react-native-unimodules';
+import { Primary } from './app/views/primary';
+import { Dashboard } from './app/views/dashboard';
+import { Settings } from './app/views/settings';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    var path = `${FileSystem.documentDirectory}/info.json`;
-    var content = `TestContent 1`;
-    console.log(path);
-    FileSystem.deleteAsync(path);
+const AppNavigator = createStackNavigator(
+  {
+    primary: { screen: Primary },
+    dashboard: { screen: Dashboard },
+    settings: { screen: Settings, }
+  },
+  {
+    defaultNavigationOptions: {
+      headerShown: false
+    },
+    initialRouteName: "primary"
   }
+);
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default createAppContainer(AppNavigator);
