ed25a482d4cbe06ab3db2b71678178cecee191c0
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-02-24 19:47:02 -0500

    Removed redundant code

--- CPP/JSON/main.cpp
+++ CPP/JSON/main.cpp
@@ -8,17 +8,15 @@ using namespace json;
 int main() {
 	std::cout << "Hello, JSON!" << std::endl;
 
-	json_object d = json_util::read("data.json");
+	json_data d = json_util::read("data.json");
 	//std::cout << d.getType() << ' ' << d.stringify() << std::endl;
 
 	//json_util::write("data2.json", d);
 
-	json_data test { {"hello", 5 } };
+	//d["json"] = { {"hello", 5 } };
+	d["json"]["hello"]["he"] = 7;
 
-	d["json"] = { {"hello", 5 } };
-	d["json"]["hello"] = 7;
-
-	std::cout << d["json"]["hello"].stringify() << std::endl;
+	std::cout << d.stringify() << std::endl;
 
 	std::cout << "Goodbye, JSON!" << std::endl;
 }
