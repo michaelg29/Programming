e78e3947760246342534a9701d467d29488c8e82
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-03-03 19:13:43 -0500

    added dll test library

--- CPP/Matrix/Matrix/main.cpp
+++ CPP/Matrix/Matrix/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "MathLibrary.h"
 
-int main() {
-	std::cout << "Hello, world!" << std::endl;
+int main()
+{
+    // Initialize a Fibonacci relation sequence.
+    fibonacci_init(1, 1);
+    // Write out the sequence values until overflow.
+    do {
+        std::cout << fibonacci_index() << ": "
+            << fibonacci_current() << std::endl;
+    } while (fibonacci_next());
+    // Report count of values written before overflow.
+    std::cout << fibonacci_index() + 1 <<
+        " Fibonacci sequence values fit in an " <<
+        "unsigned 64-bit integer." << std::endl;
 
-	system("pause");
-	return 0;
+    system("pause");
+
+    return 0;
 }
