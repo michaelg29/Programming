cfb5a619390f2c13e42006ffd59ff07b5ae4bfe3
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-03-09 22:15:44 -0400

    Added routes and prototyped route library

---
+++ Mobile/Flutter/tutorial/lib/args.dart
@@ -0,0 +1,30 @@
+library tutorial.args;
+
+import 'package:flutter/material.dart';
+
+class Args {
+  final String username;
+  final String password;
+
+  Args(this.username, this.password);
+}
+
+class ArgsScreen extends StatelessWidget {
+  static const routeName = '/extractArguments';
+
+  final Map<String, dynamic> args;
+
+  ArgsScreen({Key key, @required this.args}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args["username"]),
+      ),
+      body: Center(
+        child: Text(args["password"]),
+      ),
+    );
+  }
+}
