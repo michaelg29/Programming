bc9bfda707abc05fd5f2df99bb614d9250bbd6c6
michaelg29 <michaelgriecocoding@gmail.com>
2020-03-12 12:15:29 -0400

    Coordinate system

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aColor;
-layout (location = 2) in vec2 aTexCoord;
+//layout (location = 1) in vec3 aColor;
+layout (location = 1) in vec2 aTexCoord;
 
 //uniform float offset;
 
@@ -10,10 +10,16 @@ out vec2 TexCoord;
 
 uniform mat4 transform;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
 	//gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
-	gl_Position = transform * vec4(aPos, 1.0f);
-	ourColor = aColor;
+	//gl_Position = transform * vec4(aPos, 1.0f);
+	//gl_Position = vec4(aPos, 1.0f);
+	gl_Position = projection * view * model * vec4(aPos, 1.0);
+	//ourColor = aColor;
 	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
