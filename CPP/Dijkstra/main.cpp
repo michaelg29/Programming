8b002136dcf70336697453776325ae9a3178771b
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-03-16 13:15:08 -0400

    Updated Dijkstra's project

---
+++ CPP/Dijkstra/main.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+#include "node.h"
+
+node* dijkstra(node* start);
+
+template<typename T>
+bool contains(std::vector<T*> v, T* x);
+
+node* findMinOnQueue(std::vector<std::pair<node*, double>> queue);
+int findOnQueue(std::vector<std::pair<node*, double>> queue, node* element);
+double calculateDistance(std::map<node*, double> weight, std::map<node*, node*> prevNode, node* element);
+double calculateDistance(node* shortestPathsTree, node* goal);
+node* generateTree(std::map<node*, double> weight, std::map<node*, node*> prevNode, node* n);
+
+int main() {
+	std::cout << "Hello, Dijkstra!" << std::endl;
+
+	node* A = node::genNode("A");
+	node* B = node::genNode("B");
+	node* C = node::genNode("C");
+	node* D = node::genNode("D");
+	node* E = node::genNode("E");
+	node* F = node::genNode("F");
+	node* G = node::genNode("G");
+
+	A->neighbors.push_back({ B, 5 });
+	A->neighbors.push_back({ C, 3 });
+	B->neighbors.push_back({ C, 2 });
+	B->neighbors.push_back({ E, 3 });
+	B->neighbors.push_back({ G, 1 });
+	C->neighbors.push_back({ D, 7 });
+	C->neighbors.push_back({ E, 7 });
+	D->neighbors.push_back({ A, 2 });
+	D->neighbors.push_back({ F, 6 });
+	E->neighbors.push_back({ D, 2 });
+	E->neighbors.push_back({ F, 1 });
+	G->neighbors.push_back({ E, 1 });
+
+	node* tree = dijkstra(A);
+	std::cout << calculateDistance(tree, D) << std::endl;
+
+	return 0;
+}
+
+node* dijkstra(node* start) {
+	std::map<node*, double> weight;
+	std::map<node*, node*> prevNode;
+
+	weight[start] = 0;
+	prevNode[start] = start;
+
+	std::vector<node*> visited;
+
+	std::vector<std::pair<node*, double>> queue;
+	queue.push_back({ start, 0 });
+
+	while (queue.size() != 0) {
+		node* smallest = findMinOnQueue(queue);
+
+		visited.push_back(smallest);
+		queue.erase(queue.begin() + findOnQueue(queue, smallest));
+
+		for (std::pair<node*, double> neighbor : smallest->neighbors) {
+			auto loc = findOnQueue(queue, neighbor.first);
+
+			if (contains<node>(visited, neighbor.first)) {
+				// already visited
+				continue;
+			}
+			else if (loc != -1) {
+				// in queue, find new path
+				double currentDistance = calculateDistance(weight, prevNode, neighbor.first);
+
+				double newDistance = calculateDistance(weight, prevNode, smallest) + smallest->distance(neighbor.first);
+				if (newDistance < currentDistance) {
+					// accept path
+					queue.erase(queue.begin() + loc);
+					queue.push_back({ neighbor.first, newDistance });
+					weight[neighbor.first] = smallest->distance(neighbor.first);
+					prevNode[neighbor.first] = smallest;
+				}
+			}
+			else {
+				// add to queue
+				weight[neighbor.first] = smallest->distance(neighbor.first);
+				prevNode[neighbor.first] = smallest;
+
+				queue.push_back({ neighbor.first, calculateDistance(weight, prevNode, neighbor.first) });
+			}
+		}
+	}
+
+	// generate tree
+	node* shortestPathsTree = generateTree(weight, prevNode, start);
+
+	return shortestPathsTree;
+}
+
+template<typename T>
+bool contains(std::vector<T*> v, T* x) {
+	return std::find(v.begin(), v.end(), x) != v.end();
+}
+
+node* generateTree(std::map<node*, double> weight, std::map<node*, node*> prevNode, node* n) {
+	node* ret = node::genNode(n->name);
+
+	// each node only has one path pointing to it
+	for (std::pair<node*, node*> pair : prevNode) {
+		if (*(pair.second) == *ret && *(pair.first) != *ret) {
+			node* neighbor = generateTree(weight, prevNode, pair.first);
+			ret->neighbors.push_back({ neighbor, weight[pair.first] });
+		}
+	}
+
+	return ret;
+}
+
+node* findMinOnQueue(std::vector<std::pair<node*, double>> queue) {
+	double min = queue.at(0).second;
+	node* ret = queue.at(0).first;
+
+	for (int i = 1, size = queue.size(); i < size; i++) {
+		if (queue.at(i).second < min) {
+			// found new min
+			min = queue.at(i).second;
+			ret = queue.at(i).first;
+		}
+	}
+
+	return ret;
+}
+
+int findOnQueue(std::vector<std::pair<node*, double>> queue, node* element) {
+	for (int i = 0, size = queue.size(); i < size; i++) {
+		if (*(queue.at(i).first) == *element) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+double calculateDistance(std::map<node*, double> weight, std::map<node*, node*> prevNode, node* element) {
+	if (*(prevNode[element]) == *element) {
+		// distance from element to self is 0
+		return 0;
+	}
+
+	return weight[element] + calculateDistance(weight, prevNode, prevNode[element]);
+}
+
+double calculateDistance(node* shortestPathsTree, node* goal) {
+	if (*shortestPathsTree == *goal) {
+		return 0;
+	}
+
+	double ret = 0;
+
+	for (std::pair<node*, double> pair : shortestPathsTree->neighbors) {
+		double w = calculateDistance(pair.first, goal);
+		if (w != -1) {
+			return shortestPathsTree->distance(pair.first) + w;
+		}
+	}
+
+	return -1;
+}
