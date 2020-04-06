f578d0078e00d237cfddf3213fbd850805b1063a
Michael Grieco <michaelgriecocoding@gmail.com>
2020-04-05 23:54:23 -0400

    Fixed camera up

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/io/camera.cpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/io/camera.cpp
@@ -3,6 +3,7 @@
 Camera::Camera(glm::vec3 position)
 	: cameraPos(position),
 	worldUp(glm::vec3(0.0f, 1.0f, 0.0f)),
+	cameraUp(glm::vec3(0.0f, 1.0f, 0.0f)),
 	yaw(-90.0f),
 	pitch(0.0f),
 	speed(2.5f),
@@ -77,7 +78,6 @@ void Camera::updateCameraVectors() {
 	cameraFront = glm::normalize(direction);
 
 	cameraRight = glm::normalize(glm::cross(cameraFront, worldUp));
-	cameraUp = glm::normalize(glm::cross(cameraRight, cameraFront));
 }
 
 float Camera::getZoom() {
