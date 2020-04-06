ec3be9f9bb7a08bad94e0b2ef438eba2bb89b5e6
michaelg29 <michaelgriecocoding@gmail.com>
2020-04-06 15:18:58 -0400

    finished lighting

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/object.fs
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/object.fs
@@ -10,19 +10,32 @@ uniform sampler2D texture1;
 
 uniform vec3 lightColor;
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 uniform float mixVal;
 
 void main() {
+	//vec4 objectColor = mix(texture(texture0, TexCoord), texture(texture1, TexCoord), mixVal);
+	vec4 objectColor = vec4(1.0, 0.5, 0.31, 1.0);
+
 	// ambient lighting
 	float ambientStrength = 0.1;
 	vec3 ambient = ambientStrength * lightColor;
-	//FragColor = vec4(ambient, 1.0) * mix(texture(texture0, TexCoord), texture(texture1, TexCoord), mixVal);
+	//FragColor = vec4(ambient, 1.0) * objectColor;
 
 	// diffuse lighting
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * lightColor;
-	FragColor = vec4(ambient + diffuse, 1.0) * mix(texture(texture0, TexCoord), texture(texture1, TexCoord), mixVal);
+	//FragColor = vec4(ambient + diffuse, 1.0) * objectColor;
+
+	// specular
+	float specularStrength = 0.5;
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+	vec3 specular = specularStrength * spec * lightColor;
+
+	FragColor = vec4((ambient + diffuse + specular) * objectColor, 1.0);
 }
