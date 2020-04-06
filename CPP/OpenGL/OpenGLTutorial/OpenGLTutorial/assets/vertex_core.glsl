c6fcddd8038aa36800c1a3bf8b10aa28a2b0020e
Michael Grieco <michaelgriecocoding@gmail.com>
2020-04-05 23:31:58 -0400

    Fixed error

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/vertex_core.glsl
@@ -9,8 +9,6 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-	//gl_Position = transform * vec4(aPos, 1.0);
-	//gl_Position = mouseTransform * vec4(aPos, 1.0);
 	TexCoord = aTexCoord;
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
