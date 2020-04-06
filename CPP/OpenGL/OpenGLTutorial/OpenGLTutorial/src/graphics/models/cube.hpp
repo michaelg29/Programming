79947f3ad92b8474b754208a06435062f0bdd6b9
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-04-05 23:51:29 -0400

    added position and size to cube

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/graphics/models/cube.hpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/graphics/models/cube.hpp
@@ -10,10 +10,11 @@ public:
 
 	Mesh mesh;
 
-	glm::vec3 pos = glm::vec3(0, 0, 0);
-	glm::vec3 size = glm::vec3(1.0, 1.0, 1.0);
+	glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f);
 
-	Cube() {}
+	Cube(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f)) 
+		: pos(pos), size(size) {}
 
 	void init() {
 		int noVertices = 36;
@@ -76,6 +77,11 @@ public:
 	}
 
 	void render(Shader shader) {
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, pos);
+		model = glm::scale(model, size);
+		shader.setMat4("model", model);
+
 		mesh.draw(shader);
 	}
 };
