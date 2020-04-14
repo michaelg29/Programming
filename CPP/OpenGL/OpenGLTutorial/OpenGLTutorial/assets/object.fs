11b0d199b68b6ba9693b5e9903cb06012479d244
michaelg29 <michaelgriecocoding@gmail.com>
2020-04-13 20:39:44 -0400

    Directional lighting

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/object.fs
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/assets/object.fs
@@ -6,7 +6,8 @@ struct Material {
 };
 
 struct Light {
-	vec3 position;
+	//vec3 position;
+	vec3 direction; // directional light
   
     vec3 ambient;
     vec3 diffuse;
@@ -31,7 +32,8 @@ void main() {
 
 	// diffuse lighting
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(light.position - FragPos);
+	//vec3 lightDir = normalize(light.position - FragPos);
+	vec3 lightDir = normalize(-light.direction);
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = light.diffuse * (diff * vec3(texture(material.diffuse, TexCoord)));
 
