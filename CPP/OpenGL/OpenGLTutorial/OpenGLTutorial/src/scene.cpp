c5e972ee7c7737c023a96836b28de923a728e95b
michaelg29 <michaelgriecocoding@gmail.com>
2020-06-04 18:56:21 -0400

    Added light parameter

--- CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/scene.cpp
+++ CPP/OpenGL/OpenGLTutorial/OpenGLTutorial/src/scene.cpp
@@ -11,7 +11,8 @@ unsigned int Scene::scrHeight = 0;
 */
 
 // window resize
-void Scene::framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+void Scene::framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
 	glViewport(0, 0, width, height);
 	scrWidth = width;
 	scrHeight = height;
@@ -21,10 +22,10 @@ void Scene::framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 	Constructor
 */
 Scene::Scene(int glfwVersionMajor, int glfwVersionMinor,
-	const char* title, unsigned int _scrWidth, unsigned int _scrHeight)
+			 const char *title, unsigned int _scrWidth, unsigned int _scrHeight)
 	: glfwVersionMajor(glfwVersionMajor), glfwVersionMinor(glfwVersionMinor),
-	title(title),
-	activeCamera(-1) 
+	  title(title),
+	  activeCamera(-1)
 {
 	scrWidth = _scrWidth;
 	scrHeight = _scrHeight;
@@ -32,7 +33,8 @@ Scene::Scene(int glfwVersionMajor, int glfwVersionMinor,
 	setWindowColor(0.1f, 0.15f, 0.15f, 1.0f);
 }
 
-bool Scene::init() {
+bool Scene::init()
+{
 	glfwInit();
 
 	// set version
@@ -47,13 +49,15 @@ bool Scene::init() {
 
 	// initialize window
 	window = glfwCreateWindow(scrWidth, scrHeight, title, NULL, NULL);
-	if (window == NULL) { // not created
+	if (window == NULL)
+	{ // not created
 		return false;
 	}
 	glfwMakeContextCurrent(window);
 
 	// set GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -91,8 +95,10 @@ bool Scene::init() {
 	main loop methods
 */
 // process input
-void Scene::processInput(float dt) {
-	if (activeCamera != -1 && activeCamera < cameras.size()) {
+void Scene::processInput(float dt)
+{
+	if (activeCamera != -1 && activeCamera < cameras.size())
+	{
 		// active camera exists
 
 		// set camera direction
@@ -102,29 +108,35 @@ void Scene::processInput(float dt) {
 		cameras[activeCamera]->updateCameraZoom(Mouse::getScrollDY());
 
 		// set camera pos
-		if (Keyboard::key(GLFW_KEY_W)) {
+		if (Keyboard::key(GLFW_KEY_W))
+		{
 			cameras[activeCamera]->updateCameraPos(CameraDirection::FORWARD, dt);
 		}
-		if (Keyboard::key(GLFW_KEY_S)) {
+		if (Keyboard::key(GLFW_KEY_S))
+		{
 			cameras[activeCamera]->updateCameraPos(CameraDirection::BACKWARD, dt);
 		}
-		if (Keyboard::key(GLFW_KEY_D)) {
+		if (Keyboard::key(GLFW_KEY_D))
+		{
 			cameras[activeCamera]->updateCameraPos(CameraDirection::RIGHT, dt);
 		}
-		if (Keyboard::key(GLFW_KEY_A)) {
+		if (Keyboard::key(GLFW_KEY_A))
+		{
 			cameras[activeCamera]->updateCameraPos(CameraDirection::LEFT, dt);
 		}
-		if (Keyboard::key(GLFW_KEY_SPACE)) {
+		if (Keyboard::key(GLFW_KEY_SPACE))
+		{
 			cameras[activeCamera]->updateCameraPos(CameraDirection::UP, dt);
 		}
-		if (Keyboard::key(GLFW_KEY_LEFT_SHIFT)) {
+		if (Keyboard::key(GLFW_KEY_LEFT_SHIFT))
+		{
 			cameras[activeCamera]->updateCameraPos(CameraDirection::DOWN, dt);
 		}
 
 		// set matrices
 		view = cameras[activeCamera]->getViewMatrix();
 		projection = glm::perspective(
-			glm::radians(cameras[activeCamera]->getZoom()),	// FOV
+			glm::radians(cameras[activeCamera]->getZoom()), // FOV
 			(float)scrWidth / (float)scrHeight,				// aspect ratio
 			0.1f, 100.0f									// near and far bounds
 		);
@@ -135,20 +147,23 @@ void Scene::processInput(float dt) {
 }
 
 // update screen before each frame
-void Scene::update() {
+void Scene::update()
+{
 	glClearColor(bg[0], bg[1], bg[2], bg[3]);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 // update screen after each frame
-void Scene::newFrame() {
+void Scene::newFrame()
+{
 	// send new frame to window
 	glfwSwapBuffers(window);
 	glfwPollEvents();
 }
 
 // set uniform shader variables (lighting, etc)
-void Scene::render(Shader shader) {
+void Scene::render(Shader shader, bool applyLighting)
+{
 	shader.activate();
 
 	// set camera values
@@ -156,36 +171,44 @@ void Scene::render(Shader shader) {
 	shader.setMat4("projection", projection);
 	shader.set3Float("viewPos", cameraPos);
 
-	/*
-		lighting
-	*/
+	if (applyLighting)
+	{
+		/*
+			lighting
+		*/
 
-	// point lights
-	unsigned int noLights = pointLights.size();
-	unsigned int noActiveLights = 0;
-	for (unsigned int i = 0; i < noLights; i++) {
-		if ((activePointLights & (1 << i)) == (1 << i)) {
-			pointLights[i]->render(shader, noActiveLights);
-			noActiveLights++;
+		// point lights
+		unsigned int noLights = pointLights.size();
+		unsigned int noActiveLights = 0;
+		for (unsigned int i = 0; i < noLights; i++)
+		{
+			if ((activePointLights & (1 << i)) == (1 << i))
+			{
+				pointLights[i]->render(shader, noActiveLights);
+				noActiveLights++;
+			}
 		}
-	}
-	shader.setInt("noPointLights", noActiveLights);
+		shader.setInt("noPointLights", noActiveLights);
 
-	// spot lights
-	noLights = spotLights.size();
-	noActiveLights = 0;
- 	for (unsigned int i = 0; i < noLights; i++) {
-		if ((activeSpotLights & (1 << i)) == (1 << i)) {
-			spotLights[i]->render(shader, noActiveLights);
-			noActiveLights++;
+		// spot lights
+		noLights = spotLights.size();
+		noActiveLights = 0;
+		for (unsigned int i = 0; i < noLights; i++)
+		{
+			if ((activeSpotLights & (1 << i)) == (1 << i))
+			{
+				spotLights[i]->render(shader, noActiveLights);
+				noActiveLights++;
+			}
 		}
-	}
-	shader.setInt("noSpotLights", noActiveLights);
+		shader.setInt("noSpotLights", noActiveLights);
 
-	dirLight->render(shader);
+		dirLight->render(shader);
+	}
 }
 
-void Scene::cleanup() {
+void Scene::cleanup()
+{
 	glfwTerminate();
 }
 
@@ -193,11 +216,13 @@ void Scene::cleanup() {
 	Accesors
 */
 
-bool Scene::shouldClose() {
+bool Scene::shouldClose()
+{
 	return glfwWindowShouldClose(window);
 }
 
-Camera* Scene::getActiveCamera() {
+Camera *Scene::getActiveCamera()
+{
 	return activeCamera == -1 ? nullptr : cameras[activeCamera];
 }
 
@@ -205,11 +230,13 @@ Camera* Scene::getActiveCamera() {
 	Modifiers
 */
 
-void Scene::setShouldClose(bool shouldClose) {
+void Scene::setShouldClose(bool shouldClose)
+{
 	glfwSetWindowShouldClose(window, shouldClose);
 }
 
-void Scene::setWindowColor(float r, float g, float b, float a) {
+void Scene::setWindowColor(float r, float g, float b, float a)
+{
 	bg[0] = r;
 	bg[1] = g;
 	bg[2] = b;
