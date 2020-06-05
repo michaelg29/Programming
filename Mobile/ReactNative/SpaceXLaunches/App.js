2d0139967e91fc5ae341404e3ba3331209340b1e
michaelg29 <michaelgriecocoding@gmail.com>
2020-06-05 16:55:12 -0400

    Created project

---
+++ Mobile/ReactNative/SpaceXLaunches/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
