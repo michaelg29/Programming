0724196219e5c621bf502e79fd41b8654aa0d7df
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-06-15 14:12:20 -0400

    Added inheritability

--- Python/webscraper/main.py
+++ Python/webscraper/main.py
@@ -4,12 +4,29 @@ from htmldataparser import HtmlDataParser, RegexSelectors
 
 if __name__ == "__main__":
     formats = json.load(open(r"C:\Users\Michael Grieco\Dropbox\format.json"))
-
     data = HtmlDataParser(formats["url"], formats["data"])
-    res = data.request("search", {
-        "startYr": "2020",
-        "endYr": "2021"
-    }, {
-        "term": "term"
-    })
-    print(res["no_pages"])
+
+    startYr = 2000
+    while True:
+        start = input("Input starting year: ")
+        if len(start) == 4:
+            startYr = int(start)
+            break
+        elif len(start) == 2:
+            startYr = int("20" + start)
+            break
+        else:
+            print("Invalid input")
+
+    while True:
+        code = input("Input course subject and code: ")
+        if (code == "stop"):
+            break
+        code = re.sub(r"[ ]", '-', code)
+    
+        res = data.request("course", {
+            "startYr": str(startYr),
+            "endYr": str(startYr + 1),
+            "code": code
+        })
+        print(res["desc"]["text"])
