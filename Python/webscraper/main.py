d72d033634510557d810c01320faaf0d307136f1
michaelg29 <michaelgriecocoding@gmail.com>
2020-06-16 16:27:05 -0400

    updated code

--- Python/webscraper/main.py
+++ Python/webscraper/main.py
@@ -3,30 +3,40 @@ import re
 from htmldataparser import HtmlDataParser, RegexSelectors
 
 if __name__ == "__main__":
-    formats = json.load(open(r"C:\Users\Michael Grieco\Dropbox\format.json"))
+    formats = json.load(open(r"pipformat.json"))
     data = HtmlDataParser(formats["url"], formats["data"])
 
-    startYr = 2000
-    while True:
-        start = input("Input starting year: ")
-        if len(start) == 4:
-            startYr = int(start)
-            break
-        elif len(start) == 2:
-            startYr = int("20" + start)
-            break
-        else:
-            print("Invalid input")
+    # startYr = 2000
+    # while True:
+    #     start = input("Input starting year: ")
+    #     if len(start) == 4:
+    #         startYr = int(start)
+    #         break
+    #     elif len(start) == 2:
+    #         startYr = int("20" + start)
+    #         break
+    #     else:
+    #         print("Invalid input")
+
+    # while True:
+    #     code = input("Input course subject and code: ")
+    #     if code == "stop":
+    #         break
+    #     code = re.sub(r"[ ]", '-', code)
+    
+    #     res = data.request("course", {
+    #         "startYr": str(startYr),
+    #         "endYr": str(startYr + 1),
+    #         "code": code
+    #     })
+    #     print(res["desc"]["text"])
 
     while True:
-        code = input("Input course subject and code: ")
-        if (code == "stop"):
+        search = input("Search term: ")
+        if search == "stop":
             break
-        code = re.sub(r"[ ]", '-', code)
-    
-        res = data.request("course", {
-            "startYr": str(startYr),
-            "endYr": str(startYr + 1),
-            "code": code
+
+        res = data.request("search", query={
+            "term": search
         })
-        print(res["desc"]["text"])
+        print(res)
