a2e5a4eca1aa09909d93bd9055b84e8a6e01e20f
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-07-23 17:52:33 -0400

    Updated function names

--- CPP/OpenGL/perlin/image.hpp
+++ CPP/OpenGL/perlin/image.hpp
@@ -5,13 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-class Image {
+class Image
+{
 public:
-	bool read(const char* file) {
+	bool read(const char *file)
+	{
 		// open file
-		FILE* inptr;
+		FILE *inptr;
 		errno_t error = fopen_s(&inptr, file, "r");
-		if (error != 0) {
+		if (error != 0)
+		{
 			return false;
 		}
 
@@ -32,17 +35,18 @@ public:
 		int rowSize = width * sizeof(RGBTRIPLE) + padding;
 
 		// read data row by row
-		unsigned char* row = new unsigned char[rowSize];
-		data = new RGBTRIPLE * [height];
-		for (int i = 0; i < height; i++) {
+		unsigned char *row = new unsigned char[rowSize];
+		data = new RGBTRIPLE *[height];
+		for (int i = 0; i < height; i++)
+		{
 			data[i] = new RGBTRIPLE[width];
 			fread(row, sizeof(unsigned char), rowSize, inptr);
-			for (int j = 0; j < width; j++) {
+			for (int j = 0; j < width; j++)
+			{
 				data[i][j] = {
 					row[j * 3],
 					row[j * 3 + 1],
-					row[j * 3 + 2]
-				};
+					row[j * 3 + 2]};
 			}
 		}
 
@@ -52,11 +56,13 @@ public:
 		return true;
 	}
 
-	bool write(const char* file) {
+	bool write(const char *file)
+	{
 		// open file
-		FILE* outptr;
+		FILE *outptr;
 		errno_t error = fopen_s(&outptr, file, "w");
-		if (error != 0) {
+		if (error != 0)
+		{
 			return false;
 		}
 
@@ -66,7 +72,7 @@ public:
 
 		// set/write headers
 		BITMAPFILEHEADER bf;
-		bf.bfType = 0x4D42;	// "BM"
+		bf.bfType = 0x4D42; // "BM"
 		bf.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + imgSize;
 		bf.bfReserved1 = 0;
 		bf.bfReserved2 = 0;
@@ -89,13 +95,16 @@ public:
 		fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
 		// write data
-		for (int i = 0; i < height; i++) {
+		for (int i = 0; i < height; i++)
+		{
 			// write row
 			fwrite(&data[i][0], sizeof(RGBTRIPLE), width, outptr);
 
 			// write padding
-			if (padding > 0) {
-				for (int k = 0; k < padding; k++) {
+			if (padding > 0)
+			{
+				for (int k = 0; k < padding; k++)
+				{
 					fputc(0x00, outptr);
 				}
 			}
@@ -107,8 +116,10 @@ public:
 		return true;
 	}
 
-	void clear() {
-		for (int i = 0; i < height; i++) {
+	void clear()
+	{
+		for (int i = 0; i < height; i++)
+		{
 			data[i] = nullptr;
 		}
 		data = nullptr;
@@ -117,44 +128,54 @@ public:
 		height = 0;
 	}
 
-	void generate(LONG _width, LONG _height) {
+	void generate(LONG _width, LONG _height)
+	{
 		width = _width;
 		height = _height;
 
-		data = new RGBTRIPLE * [height];
-		for (int i = 0; i < height; i++) {
+		data = new RGBTRIPLE *[height];
+		for (int i = 0; i < height; i++)
+		{
 			data[i] = new RGBTRIPLE[width];
 		}
 	}
 
-	bool inBounds(int x, int y) {
-		return x < width&& y < height;
+	bool inBounds(int x, int y)
+	{
+		return x < width && y < height;
 	}
 
-	bool setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b) {
-		if (!inBounds(x, y)) {
+	bool setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b)
+	{
+		if (!inBounds(x, y))
+		{
 			return false;
 		}
-		data[y][x] = { b, g, r };
+		data[y][x] = {b, g, r};
 		return true;
 	}
 
-	bool setPixel(int x, int y, double r, double g, double b) {
+	bool setPixel(int x, int y, double r, double g, double b)
+	{
 		return setPixel(x, y, r * 255 + 0.5, g * 255 + 0.5, b * 255 + 0.5);
 	}
 
-	void scale(float f) {
+	void scale(float f)
+	{
 		horizontalScale(f);
 		verticalScale(f);
 	}
 
-	void scale(float fx, float fy) {
+	void scale(float fx, float fy)
+	{
 		horizontalScale(fx);
 		verticalScale(fy);
 	}
 
-	void horizontalScale(float f) {
-		if (f == 1.0f) {
+	void horizontalScale(float f)
+	{
+		if (f == 1.0f)
+		{
 			return;
 		}
 
@@ -164,24 +185,30 @@ public:
 		width *= f;
 
 		// allocate new array
-		RGBTRIPLE** newData = new RGBTRIPLE * [height];
-		for (int i = 0; i < height; i++) {
+		RGBTRIPLE **newData = new RGBTRIPLE *[height];
+		for (int i = 0; i < height; i++)
+		{
 			newData[i] = new RGBTRIPLE[width];
 		}
 
 		// load modified data into new array
-		for (int row = 0; row < height; row++) {
+		for (int row = 0; row < height; row++)
+		{
 			for (int newi = 0, oldi = 0;
-				newi < width;
-				newi++, oldi++) {
-				if (f < 1.0f) {
+				 newi < width;
+				 newi++, oldi++)
+			{
+				if (f < 1.0f)
+				{
 					// skip cols
 					newData[row][newi] = data[row][oldi];
 					oldi += 1 / f - 1;
 				}
-				else {
+				else
+				{
 					// copy cols
-					for (int j = 0; j < f; j++) {
+					for (int j = 0; j < f; j++)
+					{
 						newData[row][newi++] = data[row][oldi];
 					}
 					newi--;
@@ -193,8 +220,10 @@ public:
 		data = newData;
 	}
 
-	void verticalScale(float f) {
-		if (f == 1.0f) {
+	void verticalScale(float f)
+	{
+		if (f == 1.0f)
+		{
 			return;
 		}
 
@@ -204,23 +233,28 @@ public:
 		height *= f;
 
 		// allocate new array
-		RGBTRIPLE** newData = new RGBTRIPLE * [height];
-		for (int i = 0; i < height; i++) {
+		RGBTRIPLE **newData = new RGBTRIPLE *[height];
+		for (int i = 0; i < height; i++)
+		{
 			newData[i] = new RGBTRIPLE[width];
 		}
 
 		// load modified data into new array
 		for (int newi = 0, oldi = 0;
-			newi < height;
-			newi++, oldi++) {
-			if (f < 1.0f) {
+			 newi < height;
+			 newi++, oldi++)
+		{
+			if (f < 1.0f)
+			{
 				// skip rows
 				newData[newi] = data[oldi];
 				oldi += 1 / f - 1;
 			}
-			else {
+			else
+			{
 				// copy rows
-				for (int j = 0; j < f; j++) {
+				for (int j = 0; j < f; j++)
+				{
 					newData[newi++] = data[oldi];
 				}
 				newi--;
@@ -231,37 +265,75 @@ public:
 		data = newData;
 	}
 
-	void rotate90CW() {
-		rotate();
+	void rotate90CW()
+	{
+		reflectXY();
 		reflectX();
 	}
 
-	void rotate90CCW() {
-		rotate();
+	void rotate90CCW()
+	{
+		reflectXY();
 		reflectY();
 	}
 
-	void rotate180() {
+	void rotate180()
+	{
 		rotate90CW();
 		rotate90CW();
 	}
 
+	// reflect across diagonal
+	void reflectXY()
+	{
+		// update properties
+		LONG oldWidth = width;
+		LONG oldHeight = height;
+
+		width = oldHeight;
+		height = oldWidth;
+
+		// allocate new array
+		RGBTRIPLE** newData = new RGBTRIPLE * [height];
+		for (int i = 0; i < height; i++)
+		{
+			newData[i] = new RGBTRIPLE[width];
+		}
+
+		// load modified data into new array
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				newData[i][j] = data[j][i];
+			}
+		}
+
+		// set new data
+		data = newData;
+	}
+
 	// reflect across x axis (vertically)
-	void reflectX() {
+	void reflectX()
+	{
 		// update data
-		for (int i = 0; i < height / 2; i++) {
+		for (int i = 0; i < height / 2; i++)
+		{
 			// make copy of row
-			RGBTRIPLE* tmp = data[i];
+			RGBTRIPLE *tmp = data[i];
 			data[i] = data[height - 1 - i];
 			data[height - 1 - i] = tmp;
 		}
 	}
 
 	// reflect across y axis (horizontally)
-	void reflectY() {
+	void reflectY()
+	{
 		// update data
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width / 2; j++) {
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width / 2; j++)
+			{
 				// swap pixel with one on other side
 				RGBTRIPLE tmp = data[i][j];
 				data[i][j] = data[i][width - 1 - j];
@@ -274,32 +346,7 @@ private:
 	LONG width;
 	LONG height;
 
-	RGBTRIPLE** data;
-
-	void rotate() {
-		// update properties
-		LONG oldWidth = width;
-		LONG oldHeight = height;
-
-		width = oldHeight;
-		height = oldWidth;
-
-		// allocate new array
-		RGBTRIPLE** newData = new RGBTRIPLE * [height];
-		for (int i = 0; i < height; i++) {
-			newData[i] = new RGBTRIPLE[width];
-		}
-
-		// load modified data into new array
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				newData[i][j] = data[j][i];
-			}
-		}
-
-		// set new data
-		data = newData;
-	}
+	RGBTRIPLE **data;
 };
 
 #endif
