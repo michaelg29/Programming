8256ebc17b522ad9bed7d069a2a49db4771395bb
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-08-03 01:43:08 -0400

    Updated algorithm

--- CPP/OpenGL/perlin/perlin.hpp
+++ CPP/OpenGL/perlin/perlin.hpp
@@ -3,7 +3,6 @@
 
 #include <iterator>
 #include <algorithm>
-#include <stdlib.h>
 #include <random>
 
 namespace perlin {
@@ -11,21 +10,29 @@ namespace perlin {
 		utility methods
 	*/
 
-	// fade function: f(t) = 6t^5 - 15t^4 + 10t^3 (optimized for fewer mutliplications)
+	// fade function: f(t) = 6*t^5 - 15*t^4 + 10*t^3 (optimize for fewer multiplications)
 	inline float fade(float t) {
 		return t * t * t * (t * (t * 6 - 15) + 10);
 	}
 
-	// linear interpolation: x(t) = x1 + t * (x2 - x1)
+	// linear interpolation
 	inline float lerp(float t, float a, float b) {
 		return a + t * (b - a);
 	}
 
-	// generate gradient vector for points
-	// calculates dot product between gradient vector and position vector
-	inline float grad(int hash, float x, float y, float z) {
-		// convert last 4 bits of hash into one of 12 possible gradients
-		int h = hash & 0b1111; // = hash % 16 = get last 4 bits
+	// map function
+	inline float map(float val, float ogMin, float ogMax, float newMin, float newMax) {
+		// get proportion in original range
+		float prop = (val - ogMin) / (ogMax - ogMin);
+
+		return lerp(prop, newMin, newMax);
+	}
+
+	// gradient function
+	// calculate the dot product between the gradient vector and distance vector
+	inline float grad(unsigned char hash, float x, float y, float z) {
+		// convert the last 4 bits of the hash into one of 12 possible gradients
+		int h = hash & 0b1111; // = hash & 15 = hash % 16 = get last 4 bits
 
 		// if first bit 1, set to x, otherwise set to y
 		float u = h < 0b1000 ? x : y;
@@ -35,16 +42,10 @@ namespace perlin {
 		// else set to z
 		float v = h < 0b0100 ? y : h == 0b1100 || h == 0b1110 ? x : z;
 
-		// make u, v negative based on last 2 bits, then add results
+		// make u, v negative based on the last 2 bits, then add results
+		// adding results is like dot product because gradient components are 1,
+		// so results of dot product is adding distance components
 		return ((h & 0b0001) == 0 ? u : -u) + ((h & 0b0010) == 0 ? v : -v);
-	}
-
-	// map function
-	inline float map(float val, float ogMin, float ogMax, float newMin, float newMax) {
-		// get distance in original range
-		float prop = (val - ogMin) / (ogMax - ogMin);
-
-		return newMin + prop * (newMax - newMin);
 	}
 
 	/*
@@ -53,7 +54,7 @@ namespace perlin {
 
 	class Perlin {
 	private:
-		// permutation array
+		// permutation table
 		unsigned char p[512];
 
 		// current seed
@@ -61,23 +62,24 @@ namespace perlin {
 
 	public:
 		/*
-			constructor
+			constructor/modifiers
 		*/
-		Perlin(int seed) {
+
+		// constructor
+		Perlin(unsigned int seed) {
 			reseed(seed);
 		}
 
-		// method to reseed permutation table
+		// reseed the class
 		void reseed(unsigned int _seed) {
 			seed = _seed;
-			srand(seed);
 
-			// populate array in order
+			// populate permutation table in order
 			for (unsigned int i = 0; i < 256; i++) {
 				p[i] = i;
 			}
 
-			// shuffle first half
+			// shuffle
 			std::shuffle(std::begin(p), std::begin(p) + 256, std::default_random_engine(seed));
 
 			// duplicate array for overflow
@@ -90,36 +92,36 @@ namespace perlin {
 			perlin methods
 		*/
 
-		// 1D Perlin noise
+		// 1D Perlin Noise
 		float noise1D(float x) {
-			// find interval containing point
-			int xi = (int)(std::floorf(x)) & 255;
+			// find smallest point of interval containing target
+			int xi = (int)(std::floorf(x)) & 255; // = % 256
 
 			// get decimal value of each component
 			x -= std::floorf(x);
 
 			// get smooth value from fade function (becomes weight for each dimension)
-			float u = fade(x);
+			float sx = fade(x);
 
-			// hash function for all 4 unit square coordinates
-			int a, b;
+			// get hash value for all neighboring points
+			unsigned char a, b;
 			a = p[xi    ];
 			b = p[xi + 1];
 
 			// get weighted average
 			float avg = lerp(
-				u,
-				grad(a, x, 0, 0),
-				grad(b, x - 1, 0, 0)
+				sx,
+				grad(a, x    , 0, 0),
+				grad(a, x - 1, 0, 0)
 			);
 
-			// return avg mapped from [-1, 1] to [0, 1]
+			// return avg mapped from [-1, 1] (theoretically) to [0, 1]
 			return map(avg, -1, 1, 0, 1);
 		}
 
-		// 2D Perlin noise
+		// 2D Perlin Noise
 		float noise2D(float x, float y) {
-			// find square containing point
+			// find smallest point of square containing target
 			int xi = (int)(std::floorf(x)) & 255;
 			int yi = (int)(std::floorf(y)) & 255;
 
@@ -131,8 +133,8 @@ namespace perlin {
 			float sx = fade(x);
 			float sy = fade(y);
 
-			// hash function for all 4 unit square coordinates
-			int aa, ab, ba, bb;
+			// get hash value for all neighboring points
+			unsigned char aa, ab, ba, bb;
 			aa = p[p[xi    ] + yi    ];
 			ab = p[p[xi    ] + yi + 1];
 			ba = p[p[xi + 1] + yi    ];
@@ -141,25 +143,25 @@ namespace perlin {
 			// get weighted average
 			float avg = lerp(
 				sy,
-				lerp(
-					sx, // "top"
+				lerp( // "top"
+					sx,
 					grad(aa, x    , y    , 0),
 					grad(ba, x - 1, y    , 0)
 				),
-				lerp(
-					sx, // "bottom"
+				lerp( // "bottom"
+					sx,
 					grad(ab, x    , y - 1, 0),
 					grad(bb, x - 1, y - 1, 0)
 				)
 			);
 
-			// return avg mapped from [-1, 1] to [0, 1]
+			// return avg mapped from [-1, 1] (theoretically) to [0, 1]
 			return map(avg, -1, 1, 0, 1);
 		}
 
-		// 3D Perlin noise
+		// 3D Perlin NOise
 		float noise3D(float x, float y, float z) {
-			// find cube containing point
+			// find smallest point of cube containing target
 			int xi = (int)(std::floorf(x)) & 255;
 			int yi = (int)(std::floorf(y)) & 255;
 			int zi = (int)(std::floorf(z)) & 255;
@@ -170,11 +172,11 @@ namespace perlin {
 			z -= std::floorf(z);
 
 			// get smooth value from fade function (becomes weight for each dimension)
-			float u = fade(x);
-			float v = fade(y);
-			float w = fade(z);
+			float sx = fade(x);
+			float sy = fade(y);
+			float sz = fade(z);
 
-			// hash function for all 8 unit cube coordinates
+			// get hash value for all neighboring points
 			unsigned char aaa, aba, aab, abb, baa, bba, bab, bbb;
 			aaa = p[p[p[xi    ] + yi    ] + zi    ];
 			aba = p[p[p[xi    ] + yi + 1] + zi    ];
@@ -187,37 +189,36 @@ namespace perlin {
 
 			// get weighted average
 			float avg = lerp(
-				w,
-				lerp(
-					v, // "front"
-					lerp(
-						u, // "top"
+				sz,
+				lerp( // "front"
+					sy,
+					lerp( // "top"
+						sx,
 						grad(aaa, x    , y    , z    ),
 						grad(baa, x - 1, y    , z    )
 					),
-					lerp(
-						u, // "bottom"
+					lerp( // "bottom"
+						sx,
 						grad(aba, x    , y - 1, z    ),
 						grad(bba, x - 1, y - 1, z    )
 					)
 				),
-				lerp(
-					// "rear"
-					v,
-					lerp(
-						u, // "top"
+				lerp( // "rear"
+					sy,
+					lerp( // "top"
+						sx,
 						grad(aab, x    , y    , z - 1),
-						grad(bab, x - 1, y    , z - 1)
+						grad(bab, x - 1, y    , z    )
 					),
-					lerp(
-						u, // "bottom"
+					lerp( // "bottom"
+						sx,
 						grad(abb, x    , y - 1, z - 1),
 						grad(bbb, x - 1, y - 1, z - 1)
 					)
 				)
 			);
 
-			// return avg mapped from [-1, 1] to [0, 1]
+			// return avg mapped from [-1, 1] (theoretically) to [0, 1]
 			return map(avg, -1, 1, 0, 1);
 		}
 
@@ -226,7 +227,7 @@ namespace perlin {
 		*/
 
 		// 1D accumulated noise
-		float accumulatedOctaveNoise1D(float x, int octaves = 8, float lacunarity = 2.0f, float gain = 0.5f) {
+		float accumulatedNoise1D(float x, int octaves = 8, float lacunarity = 2.0f, float gain = 0.5f) {
 			float result = 0.0f;
 			float amplitude = 1.0f;
 			float frequency = 1.0f;
@@ -241,11 +242,12 @@ namespace perlin {
 				frequency *= lacunarity;
 			}
 
+			// return normalized result
 			return result / maxVal;
 		}
 
 		// 2D accumulated noise
-		float accumulatedOctaveNoise2D(float x, float y, int octaves = 8, float lacunarity = 2.0f, float gain = 0.5f) {
+		float accumulatedNoise2D(float x, float y, int octaves = 8, float lacunarity = 2.0f, float gain = 0.5f) {
 			float result = 0.0f;
 			float amplitude = 1.0f;
 			float frequency = 1.0f;
@@ -253,18 +255,19 @@ namespace perlin {
 
 			for (; octaves > 0; octaves--) {
 				result += noise2D(x * frequency, y * frequency) * amplitude;
-				
+
 				maxVal += amplitude;
 
 				amplitude *= gain;
 				frequency *= lacunarity;
 			}
 
+			// return normalized result
 			return result / maxVal;
 		}
 
 		// 3D accumulated noise
-		float accumulatedOctaveNoise3D(float x, float y, float z, int octaves = 8, float lacunarity = 2.0f, float gain = 0.5f) {
+		float accumulatedNoise2D(float x, float y, float z, int octaves = 8, float lacunarity = 2.0f, float gain = 0.5f) {
 			float result = 0.0f;
 			float amplitude = 1.0f;
 			float frequency = 1.0f;
@@ -279,6 +282,7 @@ namespace perlin {
 				frequency *= lacunarity;
 			}
 
+			// return normalized result
 			return result / maxVal;
 		}
 	};
