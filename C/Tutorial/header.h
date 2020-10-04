f6e1b3cde4d6dd29c6051b809087e40e5ad76393
michaelg29 <michaelgriecocoding@gmail.com>
2020-10-03 22:50:58 -0400

    preprocessors

---
+++ C/Tutorial/header.h
@@ -0,0 +1,11 @@
+// #include <file> includes system headers
+// #include "file" includes files in program
+
+// to avoid multiple defintions
+#ifndef HEADER_H
+#define HEADER_H
+
+// header content
+int test();
+
+#endif
