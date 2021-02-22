dd275a376fee9d4d6628e205637e9d3cab567c40
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2021-02-22 14:25:50 -0500

    Merged project

--- C/math/cmathematics/bigint.c
+++ C/math/cmathematics/bigint.c
@@ -17,13 +17,12 @@ bigint allocateBigint(unsigned int capacity)
     ret.sign = true;
     // allocate array
     ret.digits = malloc(capacity * sizeof(int));
-
     return ret;
 }
 
 /**
- * free memory
- * @param i the integer to be freed
+ * free the memory of a big integer
+ * @param i the integer
  */
 void freeBigint(bigint *i)
 {
@@ -36,25 +35,30 @@ void freeBigint(bigint *i)
 
 /**
  * convert string to a big integer by parsing each digit
+ * 
+ * PROCESS
+ * for each block of 9 base 10 digits
+ * val = digit[8] * 10^8 + digit[7] * 10^7 + ... + digit[0] + 10^0
+ *     = ((digit[8] * 10 + digit[7]) * 10 + ... ) * 10 + digit[0]
+ * 
  * @param str the string
  * @return the integer
  */
 bigint strToBigint(char *str)
 {
-    // length of input
-    unsigned int len = strlen(str);
-    unsigned int effectiveLen = len;
-    unsigned int firstDigit = 0;
+    unsigned int len = strlen(str);  // length of raw string
+    unsigned int effectiveLen = len; // length of string to parse after discarding negative sign and leading zeros
+    unsigned int firstDigit = 0;     // index of first digit
 
     // sign placeholder
     bool sign = true;
     if (str[firstDigit] == '-')
     {
-        // first character is a negative sign
+        // first character is negative sign
         sign = false;
-        // number of digits to read is less
         effectiveLen--;
 
+        // advance cursor
         firstDigit++;
     }
 
@@ -62,6 +66,8 @@ bigint strToBigint(char *str)
     while (str[firstDigit] == '0')
     {
         effectiveLen--;
+
+        // advance cursor
         firstDigit++;
     }
 
@@ -77,7 +83,7 @@ bigint strToBigint(char *str)
 
     if (extra)
     {
-        // if remainder, need extra digit
+        // remainder, so need extra digit
         numDigits++;
     }
 
@@ -87,22 +93,23 @@ bigint strToBigint(char *str)
     ret.noDigits = numDigits;
     ret.sign = sign;
 
-    // counter for current digit
-    unsigned int currentDigit = 0;
-    char currentDigitCharIdx = extra;
-    unsigned int digitArrIdx = numDigits - 1;
+    unsigned int currentDigit = 0;            // index in bigint array
+    unsigned int digitArrIdx = numDigits - 1; // index of the current bigint digit
+
+    // begin counting down from number of digits in largest value integer
+    char currentDigitCharIdx = extra ? extra : NO_BASE_DIGITS; // current string digit
 
     // iterate through each character
     for (unsigned int idx = firstDigit; idx < len; idx++)
     {
         char c = str[idx];
 
-        // character must be between 0 and 9
+        // validate
         if (c >= '0' && c <= '9')
         {
-            // get integer value of character
-            currentDigit *= 10;
-            currentDigit += c - '0';
+            // get integer
+            currentDigit *= 10;      // increase magnitude
+            currentDigit += c - '0'; // get integer value of digit
             currentDigitCharIdx--;
 
             if (!currentDigitCharIdx)
@@ -127,7 +134,62 @@ bigint strToBigint(char *str)
 }
 
 /**
+ * creates new positive big integer from an unsigned integer
+ * 
+ * PROCESS - conversion of bases
+ * i_10 / BASE = i' R digits[0] = LSD (least significant digit)
+ * i' / BASE = i'' R digits[1]
+ * ...
+ * i''' / BASE = 0 R digits[N - 1] = MSD (most significant digit)
+ * 
+ * @param i the integer
+ * @return the integer
+ */
+bigint newPositiveBigint(unsigned int i)
+{
+    // get number of digits
+    unsigned int noDigits = numDigits(i, BASE);
+
+    bigint ret = allocateBigint(noDigits);
+    ret.noDigits = noDigits;
+    for (unsigned int idx = 0; idx < noDigits; idx++)
+    {
+        // get next digit
+        divMod(i, BASE, &i, &ret.digits[idx]);
+    }
+
+    return ret;
+}
+
+/**
+ * creates new positive big integer from an unsigned long long
+ * same process as newPositiveBigInt
+ * 
+ * @param l the num
+ * @return the big integer
+ */
+bigint newLLPositiveBigInt(unsigned long long l)
+{
+    // get number of digits
+    unsigned long long noDigits = numDigitsLL(l, BASE);
+
+    bigint ret = allocateBigint(noDigits);
+    ret.noDigits = noDigits;
+    for (unsigned int idx = 0; idx < noDigits; idx++)
+    {
+        unsigned long long digit;
+
+        // get next digit
+        divModLL(l, BASE, &l, (unsigned long long *)&digit);
+        ret.digits[idx] = digit;
+    }
+
+    return ret;
+}
+
+/**
  * creates new big integer from an integer
+ * calls newPositiveBigInt then applies sign after
  * @param i the integer
  * @return the big integer
  */
@@ -149,42 +211,103 @@ bigint newBigint(int i)
 }
 
 /**
- * creates new positive big integer from an unsigned integer
- * @param i the integer
- * @return the integer
+ * creates new big integer from a long long
+ * calls newLLPositiveBigInt then applies sign after
+ * @param i the long long
+ * @return the big integer
  */
-bigint newPositiveBigint(unsigned int i)
+bigint newLLBigInt(long long l)
 {
-    // get number of digits
-    unsigned int noDigits = numDigits(i, BASE);
-
-    bigint ret = allocateBigint(noDigits);
-    ret.noDigits = noDigits;
-    for (unsigned int idx = 0;
-         idx < noDigits;
-         idx++)
+    bool sign = true;
+    if (l < 0LL)
     {
-        divMod(i, BASE, &i, &ret.digits[idx]);
+        sign = false;
+        l *= -1LL;
     }
+
+    // pass in positive component
+    bigint ret = newLLPositiveBigInt(l);
+    // set sign
+    ret.sign = sign;
 
     return ret;
 }
 
 /**
+ * multiplies two integers together and handles possible overflow
+ * @param i1 the first integer
+ * @param i2 the second integer
+ * @return the big integer containing the product
+ */
+bigint multiplyIntInt(unsigned int i1, unsigned int i2)
+{
+    bigint ret = newLLPositiveBigInt((long long)i1 * (long long)i2);
+
+    // get product (account for overflow)
+    return ret;
+}
+
+/**
+ * trims leading zeros in the array of digits
+ * @param b the pointer to the integer to trim
+ */
+void trimBigint(bigint *b)
+{
+    // determine how many digits to remove
+    while (!b->digits[b->noDigits - 1])
+    {
+        b->noDigits--;
+    }
+
+    // copy over elements
+    int *newDigits = malloc(b->noDigits * sizeof(int));
+
+    for (unsigned int i = 0; i < b->noDigits; i++)
+    {
+        newDigits[i] = b->digits[i];
+    }
+
+    // temporary variable for swap
+    int *tmp = b->digits;
+    b->digits = newDigits;
+    newDigits = tmp;
+
+    // free old
+    free(newDigits);
+    free(tmp);
+}
+
+/**
  * get string representation of number
+ * 
+ * EXAMPLE
+ * [456789780, 123] => "123456789780"
+ * 
+ * digitIdx strIdx  char    offset
+ * 1        0       '3'     2
+ *                  '2'     1
+ *                  '1'     0 => "123"
+ * 2        3       '0'     8
+ *                  '8'     7
+ *                  ...
+ *                  '5'     1
+ *                  '4'     0 => "456789780"
+ *                               -----------
+ *                               "123456789780"
+ * 
  * @param i the pointer to the big integer
  * @return the string
  */
 char *bigintPtrToString(bigint *i)
 {
-    if (!i->noDigits)
+    if (!i || !i->noDigits)
     {
         // no digits
         return "0";
     }
 
     unsigned int noChars = (i->noDigits - 1) * NO_BASE_DIGITS;
-    // may not need all characters for last
+    // may not need all characters for the last
     unsigned int numDigitsLast = numDigits(i->digits[i->noDigits - 1], 10);
     noChars += numDigitsLast;
 
@@ -197,44 +320,45 @@ char *bigintPtrToString(bigint *i)
     // allocate string (extra character for terminator)
     char *ret = malloc((noChars + 1) * sizeof(char));
 
-    // could not allocate memory
     if (!ret)
     {
+        // could not allocate memory
         return NULL;
     }
 
     unsigned int strIdx = 0;
     if (!i->sign)
     {
-        // set first character to negative sign
+        // set first char to negative sign
         ret[strIdx++] = '-';
     }
 
-    // highest magnitude digits are last terms in the array
-    // highest magnitude digits will be the first characters in the string
-    bool highestMagnitudeDigit = true;
+    // highest magnitude digits are last in the array but first characters in string
+    char offset = numDigitsLast;
     int strDigit;
     for (int digitIdx = i->noDigits;
          digitIdx; // digitIdx != 0
          digitIdx--)
     {
-        // convert integer value to character
-        char offset = highestMagnitudeDigit ? numDigitsLast : NO_BASE_DIGITS;
-        highestMagnitudeDigit = false;
-        int increment = offset;
+        // keep track of original offset
+        char increment = offset;
 
+        // get integer value
         int digit = i->digits[digitIdx - 1];
-        while (offset)
+        while (offset--)
         {
+            // get next lowest magnitude digit in base 10
             divMod(digit, 10, &digit, &strDigit);
-            ret[strIdx + offset - 1] = '0' + strDigit;
-            offset--;
+            ret[strIdx + offset] = '0' + strDigit;
         }
 
+        offset = NO_BASE_DIGITS;
+
+        // advance cursor
         strIdx += increment;
     }
 
-    // string termination characters
+    // string termination
     ret[noChars] = '\0';
 
     return ret;
@@ -364,10 +488,7 @@ bigint addBigint(bigint i1, bigint i2)
     }
 
     // remove leading zeros
-    while (!ret.digits[ret.noDigits - 1])
-    {
-        ret.noDigits--;
-    }
+    trimBigint(&ret);
 
     if (!i1.sign && !i2.sign) // -i1 + -i2 = -(i1 + i2)
     {
@@ -458,10 +579,128 @@ bigint subtractBigint(bigint i1, bigint i2)
     }
 
     // remove leading zeros
-    while (!ret.digits[ret.noDigits - 1])
-    {
-        ret.noDigits--;
-    }
+    trimBigint(&ret);
 
     return ret;
+}
+
+/**
+ * multiplies two integers with elementary multiplication
+ * @param i1 the first integer
+ * @param i2 the second integer
+ * @return the product
+ */
+bigint multiplyBigint(bigint i1, bigint i2)
+{
+}
+
+bigint longMultiplyBigint(bigint i1, bigint i2)
+{
+    // if either are zero, the product will be zero
+    if (!compareBigint(i1, BIGINT_ZERO) ||
+        !compareBigint(i2, BIGINT_ZERO))
+    {
+        return BIGINT_ZERO;
+    }
+
+    // if both have same sign, result is positive (exclusive NOR)
+    bool sign = !(i1.sign ^ i2.sign);
+
+    // maximum number of digits is the sum of numbers of digits
+    unsigned int noDigits = i1.noDigits + i2.noDigits;
+
+    // allocate
+    bigint ret = allocateBigint(noDigits);
+    ret.noDigits = noDigits;
+
+    // set all to 0
+    for (unsigned int i = 0; i < ret.noDigits; i++)
+    {
+        ret.digits[i] = 0;
+    }
+
+    // for each digit of i1
+    for (unsigned int i = 0; i < i1.noDigits; i++)
+    {
+        int carry = 0;
+
+        // for each digit of i2
+        for (unsigned int j = 0; j < i2.noDigits; j++)
+        {
+            // get product (account for overflow)
+            // prod[0] = addition to digit[i + j]
+            // prod[1] = addition to carry
+            bigint prod = multiplyIntInt(i1.digits[i], i2.digits[j]);
+
+            // assign to digit[i + j]
+            if (prod.noDigits > 0)
+            {
+                ret.digits[i + j] += prod.digits[0];
+            }
+            ret.digits[i + j] += carry;
+
+            // determine carry for next digit
+            if (ret.digits[i + j] >= BASE)
+            {
+                carry = 1;
+                ret.digits[i + j] -= BASE;
+            }
+            else
+            {
+                // reset carry
+                carry = 0;
+            }
+
+            // add carry from product
+            if (prod.noDigits > 1)
+            {
+                carry += prod.digits[1];
+            }
+        }
+
+        // deal with leftover carry
+        if (carry)
+        {
+            ret.digits[i + i2.noDigits] += carry;
+        }
+    }
+
+    // remove leading zeros
+    trimBigint(&ret);
+
+    return ret;
+}
+
+bigint karatsubaMultiplyBigint(bigint i1, bigint i2)
+{
+    // if either are zero, the product will be zero
+    if (!compareBigint(i1, BIGINT_ZERO) ||
+        !compareBigint(i2, BIGINT_ZERO))
+    {
+        return BIGINT_ZERO;
+    }
+
+    // if both have same sign, result is positive (exclusive NOR)
+    bool sign = !(i1.sign ^ i2.sign);
+
+    // maximum number of digits is the sum of numbers of digits
+    unsigned int noDigits = i1.noDigits + i2.noDigits;
+
+    // allocate
+    bigint ret = allocateBigint(noDigits);
+    ret.noDigits = noDigits;
+
+    // set all to 0
+    for (unsigned int i = 0; i < ret.noDigits; i++)
+    {
+        ret.digits[i] = 0;
+    }
+
+    karatsubaDirect(&i1, &i2, &ret, 0, noDigits - 1);
+
+    return ret;
+}
+
+void karatsubaDirect(bigint *i1, bigint *i2, bigint *out, unsigned int first, unsigned int last)
+{
 }
