fdbb10b07f0a56ae9647bfe52991e4876aacf984
michaelg29 <michaelgriecocoding@gmail.com>
2021-10-29 10:42:35 -0400

    Added deletion function definitions to header

--- C/btree/btree.h
+++ C/btree/btree.h
@@ -29,6 +29,7 @@ btree btree_new(int m);
 void *btree_search(btree tree, int key);
 
 void btree_insert(btree *tree, int key, void *val);
+void btree_delete(btree *tree, int key);
 
 void btree_free(btree *b);
 
@@ -46,6 +47,8 @@ keyValPair btree_node_get_inorderPredecessor(btree_node *root, btree tree, int i
 btree_node *btree_node_split(btree_node *root, btree tree, btree_node *new_node, int i);
 btree_node *btree_node_insert(btree_node *root, btree tree, int key, void *val);
 
+void btree_node_merge_children(btree_node *root, btree tree, int leftChild);
+void btree_node_borrow(btree_node *root, btree tree, int base, int offset);
 int btree_node_delete(btree_node *root, btree tree, int key);
 
 void btree_node_free(btree_node *root, btree tree);
