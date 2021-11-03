f9caee8b3ec852c206b62349bcabb7a6d6c40bf0
michaelg29 <michaelgriecocoding@gmail.com>
2021-11-03 16:24:29 -0400

    Updated successor/predecessor getter

--- C/btree/btree.h
+++ C/btree/btree.h
@@ -11,11 +11,6 @@ typedef struct btree_node {
     struct btree_node **children;
 } btree_node;
 
-typedef struct {
-    int key;
-    void *val;
-} keyValPair;
-
 typedef struct btree {
     int m;
     int t;
@@ -41,8 +36,8 @@ void btree_moveKeyVal(btree_node *in, int inIdx, btree_node *out, int outIdx);
 
 btree_node *btree_node_search(btree_node *root, btree tree, int key, int *idx);
 
-keyValPair btree_node_get_inorderSuccessor(btree_node *root, btree tree, int i);
-keyValPair btree_node_get_inorderPredecessor(btree_node *root, btree tree, int i);
+btree_node *btree_node_get_inorderSuccessor(btree_node *root, btree tree, int i);
+btree_node *btree_node_get_inorderPredecessor(btree_node *root, btree tree, int i);
 
 btree_node *btree_node_split(btree_node *root, btree tree, btree_node *new_node, int i);
 btree_node *btree_node_insert(btree_node *root, btree tree, int key, void *val);
