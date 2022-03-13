ebede7fe44f780e422ad4b52e022c773126843cb
michaelg29 <michaelgriecocoding@gmail.com>
2022-03-13 17:40:43 -0400

    Created virtual program class

---
+++ CPP/OpenGL/GLMath/GLMath/src/program.h
@@ -0,0 +1,16 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+class Program {
+	virtual void load();
+
+	virtual void processInput(double dt, GLFWwindow* window);
+	virtual void render(double dt);
+
+	virtual void cleanup();
+};
+
+#endif
