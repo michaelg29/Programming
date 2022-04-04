a3045c518070e8cfac237a52d86eedfe4a018834
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-04-04 17:13:29 -0400

    Create surface class

---
+++ CPP/OpenGL/GLMath/GLMath/src/programs/3d/surface.hpp
@@ -0,0 +1,17 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+
+#include "../program.h"
+#include "../shader.h"
+#include "../vertexmemory.hpp"
+#include "../uniformmemory.hpp"
+
+#ifndef SURFACE_HPP
+#define SURFACE_HPP
+
+class Surface : public Program {
+};
+
+#endif // SURFACE_HPP
