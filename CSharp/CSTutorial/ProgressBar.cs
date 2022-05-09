78e73d0afbb8b60204825aa8fda8585c5fc5f40e
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-05-09 16:01:44 -0400

    Created progress bar class

---
+++ CSharp/CSTutorial/ProgressBar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTutorial
+{
+    class ProgressBar
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int total = 10;
+            Console.WriteLine($"Current progress: {counter}/{total}, {(float)counter / (float)total * 100}%");
+
+            while (counter < total)
+            {
+                Thread.Sleep(1000);
+                counter++;
+                Console.WriteLine($"\rCurrent progress: {counter}/{total}, {(float)counter / (float)total * 100:#.#0}%");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
