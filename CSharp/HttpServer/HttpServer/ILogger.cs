c907705621f4e4c8f7f9e5fc31464dc8a146dd50
michaelg29 <michaelgriecocoding@gmail.com>
2022-06-05 11:45:11 -0400

    Create http listener project

---
+++ CSharp/HttpServer/HttpServer/ILogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    interface ILogger
+    {
+        void LogHeader(string head, object value);
+        void LogMessage(string msg, params object[] args);
+        void CompleteLog();
+        void ClearLog();
+        void CompleteLog(object msg);
+    }
+}
