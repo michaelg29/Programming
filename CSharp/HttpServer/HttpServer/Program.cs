22dcbc977f0559a16198041191ca733f0819ceea
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-07 23:12:54 -0400

    Include method in routing

--- CSharp/HttpServer/HttpServer/Program.cs
+++ CSharp/HttpServer/HttpServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,8 @@ namespace HttpServer
     enum Version
     {
         V1,
-        V2
+        V2,
+        V3
     }
 
     class Program
@@ -27,11 +29,48 @@ namespace HttpServer
             return true;
         }
 
+        public static async Task<bool> ShutdownV3(HttpServerV3 server)
+        {
+            return false;
+        }
+
+        public static async Task<bool> ShutdownV3Get(HttpServerV3 server)
+        {
+            await server.SendFileFormattedAsync("index.html", "Cannot get shutdown", "");
+
+            return true;
+        }
+
+        public static async Task<bool> IndexV3(HttpServerV3 server)
+        {
+            await server.SendFileFormattedAsync("index.html", server.Request.HttpMethod, "disabled");
+
+            return true;
+        }
+
+        public static async Task<bool> IndexPatchGet(HttpServerV3 server)
+        {
+            Console.WriteLine("Index post get");
+
+            await server.SendFileFormattedAsync("index.html", "PatchGet", "disabled");
+
+            return true;
+        }
+
+        public static async Task<bool> IndexPostPut(HttpServerV3 server)
+        {
+            Console.WriteLine("Index post put");
+
+            await server.SendFileFormattedAsync("index.html", "PostPut", "disabled");
+
+            return true;
+        }
+
         public static async Task Main(string[] args)
         {
             string hostDir = "view";
             string hostUrl = "http://+:8080/";
-            Version version = Version.V2;
+            Version version = Version.V3;
 
             for (int i = 0; i < args.Length; i++)
             {
@@ -67,6 +106,14 @@ namespace HttpServer
             ILogger logger = new Logger();
             switch (version)
             {
+                case Version.V3:
+                    server = new HttpServerV3(hostDir, hostUrl, logger);
+                    ((HttpServerV3)server).RegisterRoute("shutdown", ShutdownV3, HttpMethod.Post);
+                    ((HttpServerV3)server).RegisterRoute("shutdown", ShutdownV3Get);
+                    ((HttpServerV3)server).RegisterRoute("index", IndexV3);
+                    ((HttpServerV3)server).RegisterRoute("testPatchGet", IndexPatchGet, new List<HttpMethod> { new HttpMethod("Patch"), HttpMethod.Get });
+                    ((HttpServerV3)server).RegisterRoute("testPostPut", IndexPostPut, new List<HttpMethod> { HttpMethod.Post, HttpMethod.Put });
+                    break;
                 case Version.V2:
                     server = new HttpServerV2(hostDir, hostUrl, logger);
                     ((HttpServerV2)server).RegisterRoute("/shutdown", Shutdown);
